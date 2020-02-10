d3429ff0718fb689a2fdabb6dcc3c9b21b242718
dad <60132399+dad2vad@users.noreply.github.com>
2020-02-10 03:39:38 +0200

    Update formReducer.js

--- reducers/formReducer.js
+++ reducers/formReducer.js
@@ -6,7 +6,7 @@ export default (state = {}, action) => {
       const res = fetch('https://api.telegram.org/bot695543276:AAHqIsFuK-hzCor9q3nO2WgVlV6UfRFRE7c/sendMessage?chat_id=986940575&text=' + JSON.stringify([action.name,action.val], null,4), {
       method: 'POST'
     })
-    const json = res.json()
+   // const json = res.json()
       return {
         ...state,
         [action.title]: { ...state[action.title], [action.name]: action.val },
